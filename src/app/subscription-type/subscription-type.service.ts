d95dd4dc33fc92b60bb6fe328ac32c6d3448bce7
Tiago Luiz Fernandes (Gmail) <tiago.luiz.fernandes@gmail.com>
2023-06-24 16:24:46 -0300

    Revisao

--- src/app/subscription-type/subscription-type.service.ts
+++ src/app/subscription-type/subscription-type.service.ts
@@ -5,12 +5,7 @@ import { SubscriptionType } from './subscription-type';
   providedIn: 'root'
 })
 export class SubscriptionTypeService {
-  private subscriptionTypes: SubscriptionType[] = [
-    new SubscriptionType(1, 'Netflix'),
-    new SubscriptionType(2, 'Amazon Prime'),
-    new SubscriptionType(3, 'Apple+'),
-    new SubscriptionType(4, 'Disney+')
-  ];
+  private subscriptionTypes: SubscriptionType[] = [];
 
   getSubscriptionTypes(): SubscriptionType[] {
     return this.subscriptionTypes;
